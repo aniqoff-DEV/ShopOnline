01f15e37b5f5219f081fe60bc58b30e2d39df87d
aniqoff-DEV <andrey.emailforwork.dev@gmail.com>
2023-12-02 07:13:43 +0300

    part 2

---
+++ ShopOnline.API/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.API.Extensions;
+using ShopOnline.API.Repositories.Contracts;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+                var products = await _productRepository.GetItems();
+                var productCatagories = await _productRepository.GetCategories();
+
+                if(products == null || productCatagories == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    var productDtos = products.ConvertToDto(productCatagories);
+
+                    return Ok(productDtos);
+                }
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    ex.Message);
+            }
+        }
+    }
+}
---
+++ ShopOnline.API/Extensions/DtoConversions.cs
@@ -0,0 +1,27 @@
+﻿using ShopOnline.API.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.API.Extensions
+{
+    public static class DtoConversions
+    {
+        public static IEnumerable<ProductDto> ConvertToDto(this IEnumerable<Product> products, 
+                                                            IEnumerable<ProductCategory> productCategories) 
+        {
+            return (from product in products
+                    join productCategory in productCategories
+                    on product.CategoryId equals productCategory.Id
+                    select new ProductDto
+                    {
+                        Id = product.Id,
+                        Name = product.Name,
+                        Description = product.Description,
+                        ImageUrl = product.ImageUrl,
+                        Price = product.Price,
+                        Quantity = product.Quantity,
+                        CategoryId = product.CategoryId,
+                        CategoryName = productCategory.Name
+                    }).ToList();
+        }
+    }
+}
